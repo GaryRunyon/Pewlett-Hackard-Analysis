a4009993235fa3fa9fc489677fd2a0d1b5f2469d
GaryRunyon <63210941+GaryRunyon@users.noreply.github.com>
2020-05-17 21:17:45 -0400

    Challenge_Part_One.

--- Data/queries.sql
+++ Data/queries.sql
@@ -57,7 +57,6 @@ CREATE TABLE titles(
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-*/
 
 --SELECT *FROM departments;
 
@@ -102,4 +101,167 @@ CREATE TABLE titles(
 --WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 --AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-__SELECT * FROM retirement_info
+--SELECT * FROM retirement_info
+
+-- Create a new retirement_info table with emp_no added
+-- First, we have to remove the previous retirement_info table
+-- With the DROP TABLE command. 
+
+
+-- Create new table for retiring employees
+--SELECT emp_no, first_name, last_name
+--INTO retirement_info
+--FROM employees
+--WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+--AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Check the table
+--SELECT * FROM retirement_info
+
+
+-- Joining departments and dept_manager tables
+--SELECT departments.dept_name,
+--	dept_manager.emp_no,
+--	dept_manager.from_date,
+--	dept_manager.to_date
+--FROM departments
+--INNER JOIN dept_manager
+--ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining reitrement_info and dept_emp tables
+--SELECT retirement_info.emp_no,
+--	retirement_info.first_name,
+--	retirement_info.last_name,
+--	dept_emp.to_date
+--FROM retirement_info
+--LEFT JOIN dept_emp
+--ON retirement_info.emp_no = dept_emp.emp_no;
+
+--Use aliases to complete merge commands
+--SELECT d.dept_name,
+--	dm.emp_no,
+--	dm.from_date,
+--	dm.to_date
+--FROM departments as d
+--INNER JOIN dept_manager as dm
+--ON d.dept_no = dm.dept_no;
+
+--Use Left Join for retirement_info and dept_emp tables
+--SELECT ri.emp_no,
+--	ri.first_name,
+--	ri.last_name,
+--de.to_date
+--INTO current_emp
+--FROM retirement_info as ri
+--LEFT JOIN dept_emp as de
+--ON ri.emp_no = de.emp_no
+--WHERE de.to_date = ('9999-01-01')
+
+--SELECT * FROM current_emp;
+
+-- GroupBy Current Employees by Department
+-- Employee count by department number
+--SELECT COUNT(ce.emp_no), de.dept_no
+--FROM current_emp as ce
+--LEFT JOIN dept_emp as de
+--ON ce.emp_no = de.emp_no
+--GROUP BY de.dept_no;
+
+-- Add Order By so we order the DeptNo's instead of
+-- having them be random.
+
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retirees_by_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+--Next, we're looking to make a master chart of retirees
+with their salaries. It'll take a couple steps to get there.
+
+SELECT *FROM retirees_by_dept;
+
+
+SELECT *FROM salaries
+ORDER BY to_date DESC;
+
+-- This is what we need but the to_dates are not correct. 
+We'll have to pull those from the employees table. 
+
+--Let's reuse the initial code we utilized to get the 
+retiree info to begin with, with a couple of modifications.
+
+"SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');"
+
+-- We'll also use a new table_name (employee_info)
+*/
+
+
+-- SELECT e.emp_no,
+-- 	e.first_name,
+-- e.last_name,
+-- 	e.gender,
+-- 	s.salary,
+-- 	de.to_date
+-- INTO employee_info
+-- FROM employees as e
+-- INNER JOIN salaries as s
+-- ON (e.emp_no = s.emp_no)
+-- INNER JOIN dept_emp as de
+-- ON (e.emp_no = de.emp_no)
+-- WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+--      AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+-- 	  AND (de.to_date = '9999-01-01');
+
+-- SELECT *FROM employee_info;
+
+-- List of managers per department
+-- SELECT  dm.dept_no,
+--         d.dept_name,
+--         dm.emp_no,
+--         ce.last_name,
+--         ce.first_name,
+--         dm.from_date,
+--         dm.to_date
+-- INTO manager_info
+-- FROM dept_manager AS dm
+--     INNER JOIN departments AS d
+--         ON (dm.dept_no = d.dept_no)
+--     INNER JOIN current_emp AS ce
+--         ON (dm.emp_no = ce.emp_no);
+
+-- SELECT *FROM manager_info;
+
+-- The final list needs only to haev the departments
+-- added to the current_emp_table. 
+
+-- SELECT *FROM current_emp;
+
+-- SELECT ce.emp_no,
+-- ce.first_name,
+-- ce.last_name,
+-- d.dept_name	
+-- INTO dept_info
+-- FROM current_emp as ce
+-- INNER JOIN dept_emp AS de
+-- ON (ce.emp_no = de.emp_no)
+-- INNER JOIN departments AS d
+-- ON (de.dept_no = d.dept_no);
+
+--MODULE COMPLETE--
+
+
+
+
+
+
+
+
+
+
---
+++ challenge_query_sql.sql
@@ -0,0 +1,53 @@
+--Challenge code 
+
+--Part 1)
+
+-- Determine retirement-eligible employees by title.
+-- This requires selecting column fields and querying
+-- data from four separate tables, combined through 
+-- inner-joins. 
+
+-- SELECT e.emp_no,
+-- e.first_name, 
+-- e.last_name, 
+-- t.title,
+-- t.from_date,
+-- s.salary
+-- INTO retirees_by_dept_summary
+-- FROM employees as e
+-- INNER JOIN salaries as s
+-- ON (e.emp_no = s.emp_no)
+-- INNER JOIN dept_emp as de
+-- ON(e.emp_no = de.emp_no)
+-- INNER JOIN titles as t
+-- ON (e.emp_no = t.emp_no)
+-- -- Next, we specify retirees by DOB and hire dates
+-- WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+-- AND (de.to_date = '9999-01-01')
+-- AND (t.to_date = '9999-01-01')
+-- -- Then, order by department as requested:
+-- ORDER BY de.dept_no;
+
+-- VIEW TABLE
+--SELECT *FROM retirees_by_dept_summary
+
+--Drop Duplicates/ Partition;
+/*SELECT emp_no, 
+	first_name, 
+	last_name, 
+	title, 
+	from_date,
+	salary
+INTO unique_retirees_by_dept
+FROM 
+(SELECT emp_no, first_name, last_name, title, from_date, salary, ROW_NUMBER() OVER 
+(PARTITION BY (emp_no)
+ORDER BY from_date DESC) rn
+FROM retirees_by_dept_summary
+) tmp WHERE rn = 1
+ORDER BY title;
+*/
+
+--VIEW TABLE
+SELECT *FROM unique_retirees_by_dept
